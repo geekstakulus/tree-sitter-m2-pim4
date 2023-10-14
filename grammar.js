c64f56c750daff195ca57e3f2933f04cbfbc6d98
Fidel H Viegas <fhv@geekstakulus.com>
2023-10-14 02:59:47 +0100

    feat: add basic constant declaration
    - Implemented basic constant declaration, which must be integer only.
    - Integer regular expression has been implemented to support decimal,
      octals and hexadecimal numbers
    - Implemented basic expressions to support integer numbers as factor

--- grammar.js
+++ grammar.js
@@ -1,5 +1,18 @@
+// helpers
 const letter = /[a-zA-Z]/;
+const  octal_digit = /[0-7]/;
 const digit = /[0-9]/;
+const  hex_digit = /[0-9A-F]/;
+
+// int_literal = digit {digit} | 
+//               octal_digit {octal_digit} ("B" | "C") | 
+//               digit {hex_digit} "H"
+const int_literal = choice(
+  seq(digit, repeat(digit)), // decimal number
+  seq(octal_digit, repeat(octal_digit), choice("B", "C")), // octal number
+  seq(digit, repeat(hex_digit), 'H') // hexadecimal number
+);
+
 // ident = letter {letter | digit}
 const identifier = seq(letter, repeat(choice(letter, digit)));
 
@@ -83,10 +96,11 @@ module.exports = grammar({
       ";"
     ),
 
-    // block = {declarations}
+    // block = {declaration}
     //         [block_body]
     //         "END"
     block: $ => seq(
+      repeat($.declaration),
       optional($.block_body),
       $.kEnd
     ),
@@ -96,13 +110,54 @@ module.exports = grammar({
       $.kBegin
     ),
 
+    // declaration = const_declaration
+    //             | type_declaration
+    //             | var_declaration
+    //             | procedure_declaration
+    //             | module_declaration
+    declaration: $ => $.const_declaration,
+
+    // const_declaration = "CONST" {constant_declaration}
+    const_declaration: $ => seq(
+      $.kConst,
+      repeat($.constant_declaration)
+    ),
+
+    // constant_declaration = ident "=" const_expression ";"
+    constant_declaration: $ => seq(
+      field("const_id", $.ident), "=", $.const_expression, ";"
+    ),
+
+    // const_expression = expression
+    const_expression: $ => $.expression,
+
+    // expression = simple_expresion [relation simple_expression]
+    expression: $ => $.simple_expression,
+
+    // simple_expression = ["+" | "-"] term {add_operator term}
+    simple_expression: $ => $.term,
+
+    // term = factor {mul_operator factor}
+    term: $ => $.factor,
+
+    // factor = number | 
+    //          string | 
+    //          set | 
+    //          designator [actual_parameters] |
+    //          "(" expression ")" |
+    //          "NOT" factor
+    factor: $ => $.number,
+
+    // number = integer | real
+    number: $ => $.integer,
+
     // ident_list = ident {"," ident}
     ident_list: $ => seq(
       $.ident,
       repeat(
         seq(
-        ",",
-        $.ident
+          ",",
+          $.ident
         )
       )
     ),
@@ -113,6 +168,7 @@ module.exports = grammar({
     kFrom: $ => "FROM",
 
     kBegin: $ => "BEGIN",
+    kConst: $ => "CONST",
 
     kImport: $ => "IMPORT",
     kModule: $ => "MODULE",
@@ -121,6 +177,7 @@ module.exports = grammar({
 
     kImplementation: $ => "IMPLEMENTATION",
 
-    ident: $ => token(identifier)
+    ident: $ => token(identifier),
+    integer: $ => token(int_literal)
   }
 });
